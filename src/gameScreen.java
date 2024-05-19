3205aca0a6ad5e24f191e82698305aa0ca6a90b3
r0nYang <yangaaron168@gmail.com>
2024-05-19 00:20:32 -0700

    Added movement for catcher

--- src/gameScreen.java
+++ src/gameScreen.java
@@ -12,12 +12,14 @@ public class gameScreen extends JPanel implements KeyListener {
     int width = 80;
     int height = 80;
 
+
     gameScreen() {
+        this.addKeyListener(this);
+        this.setFocusable(true);
         this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
         this.setBackground(Color.white);
         ballCatcher = new ImageIcon("src/net.png").getImage();
-        timer = new Timer(10, this);
-        timer.start();
+
     }
 
     public void paint(Graphics g) {
@@ -31,10 +33,19 @@ public class gameScreen extends JPanel implements KeyListener {
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            x = 1;  // move to the right
-        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {  
-            x = -1;  // move to the left
+            if (x > PANEL_WIDTH - 100) {
+                x += 0;
+            } else {
+                x += 10;
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (x < 20) {
+                x -= 0;
+            } else {
+                x -= 10;
+            }
         }
+        repaint();
     }
 
     public void keyReleased(KeyEvent e) {
